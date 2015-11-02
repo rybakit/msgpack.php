5884b742115fc9cc2fd07164925e44ad4e12a071
Eugene Leonovich <eugene.leonovich@sanoma.com>
2015-11-02 21:34:10 +0100

    Add licence header

--- tests/DataProvider.php
+++ tests/DataProvider.php
@@ -1,5 +1,19 @@
 <?php
 
+/*
+ * This file is part of the rybakit/msgpack.php package.
+ *
+ * (c) Eugene Leonovich <gen.work@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Most of the test data are borrowed from
+ * https://github.com/vsergeev/u-msgpack-python/blob/master/test_umsgpack.py
+ */
+
 namespace MessagePack\Tests;
 
 use MessagePack\Ext;
