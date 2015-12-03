f73fd6b7c00c986282d5dfe29b9af9c93fd3c900
Eugene Leonovich <eugene.leonovich@sanoma.com>
2015-12-03 12:40:25 +0100

    Install gmp extension on a docker container

--- dockerfile.sh
+++ dockerfile.sh
@@ -8,6 +8,8 @@ RUN_CMDS=''
 
 if [[ $PHP_RUNTIME == php* ]]; then
     RUN_CMDS="$RUN_CMDS && \\\\\n    docker-php-ext-install zip"
+    RUN_CMDS="$RUN_CMDS && \\\\\n    apt-get install -y libgmp-dev"
+    RUN_CMDS="$RUN_CMDS && \\\\\n    ln -s /usr/include/x86_64-linux-gnu/gmp.h /usr/include/gmp.h && docker-php-ext-install gmp"
 fi
 
 if [[ $PHPUNIT_OPTS =~ (^|[[:space:]])--coverage-[[:alpha:]] ]]; then
@@ -18,9 +20,9 @@ fi
 echo -e "
 FROM $PHP_RUNTIME
 
-RUN apt-get update && \\
-    apt-get install -y git curl zlib1g-dev${RUN_CMDS} && \\
-    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer && \\
+RUN apt-get update && apt-get install -y git curl zlib1g-dev${RUN_CMDS}
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer && \\
     composer global require 'phpunit/phpunit:^4.8|^5.0'
 
 ENV PATH=~/.composer/vendor/bin:\$PATH
